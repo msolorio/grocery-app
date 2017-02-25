266e16422249e5f1bccf65f67f9540d6aa4de522
msolorio <msolorio789@gmail.com>
2017-02-25 00:57:42 -0800

    add basic setup

--- client/components/editForm.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function editForm() {
-  return <div>from edit form</div>;
-}
-
-module.exports = editForm;
--- client/index.js
+++ client/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
 
 ReactDOM.render(
-  <App />,
+  <div>from index.js</div>,
   document.getElementById('root')
 );
