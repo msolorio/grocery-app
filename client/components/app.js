f622733b8d044beb1e03377dbc0e357e5acef251
msolorio <msolorio789@gmail.com>
2017-02-26 23:10:00 -0800

    add incrememnt / decrement quantity with arrow click for each item and move itemsArray to state for now

--- client/components/app.js
+++ client/components/app.js
@@ -7,46 +7,78 @@ import Cards from './cards';
 
 const App = React.createClass({
   getInitialState() {
-    return { viewToShow: 'cards' };
-  },
-
-  componentWillMount() {
-
-    // dummy data
-    this.itemsArray = [
-      {
-        name: 'spinach',
-        quantity: 0.7,
-        target: 1,
-        incrementor: 0.1,
-        location: 'Sprouts'
-      },
-      {
-        name: 'sweet potato',
-        quantity: 5,
-        target: 8,
-        incrementor: 1,
-        location: 'Sprouts'
-      },
-      {
-        name: 'dates',
-        quantity: 0.4,
-        target: 1,
-        incrementor: 0.1,
-        location: 'Costco'
-      }
-    ];
-
+    return {
+      viewToShow: 'cards',
+      itemsArray: [
+          {
+          name: 'spinach',
+          quantity: 0.7,
+          target: 1,
+          incrementor: 0.1,
+          location: 'Sprouts',
+          units: 'bins'
+        },
+        {
+          name: 'sweet potato',
+          quantity: 5.0,
+          target: 8,
+          incrementor: 1,
+          location: 'Sprouts',
+          unit: ''
+        },
+        {
+          name: 'dates',
+          quantity: 0.4,
+          target: 1,
+          incrementor: 0.1,
+          location: 'Costco',
+          units: 'bins'
+        }
+      ]
+    };
   },
 
   handleNavClicked(viewToShow) {
     this.setState({ viewToShow });
   },
 
+  parseQuantity(input) {
+    if (Math.round(input) !== input ) return Math.round(input * 10) / 10;
+    return input;
+  },
+
+  handleDownClicked(index) {
+    const itemsArrayClone = this.state.itemsArray.slice();
+    let quantity = itemsArrayClone[index].quantity;
+    const incrementor = itemsArrayClone[index].incrementor;
+    itemsArrayClone[index].quantity = this.parseQuantity(quantity - incrementor);
+
+    this.setState({
+      itemsArray: itemsArrayClone
+    });
+  },
+
+  handleUpClicked(index) {
+    const itemsArrayClone = this.state.itemsArray.slice();
+    let quantity = itemsArrayClone[index].quantity;
+    const incrementor = itemsArrayClone[index].incrementor;
+    itemsArrayClone[index].quantity = this.parseQuantity(quantity + incrementor);
+
+    this.setState({
+      itemsArray: itemsArrayClone
+    });
+  },
+
   renderView(viewToShow) {
     switch (viewToShow) {
       case 'cards':
-        return <Cards itemsArray={this.itemsArray} />;
+        return (
+          <Cards
+            itemsArray={this.state.itemsArray}
+            handleDownClicked={this.handleDownClicked}
+            handleUpClicked={this.handleUpClicked}
+          />
+        );
       case 'list':
         return <List />;
       case 'addItemForm':
@@ -58,7 +90,7 @@ const App = React.createClass({
 
   render() {
     return (
-      <div className="jsxApp">
+      <div className="jsxApp container">
         <Header handleNavClicked={this.handleNavClicked} />
         {this.renderView(this.state.viewToShow)}
       </div>
